3c31ea45f50146b9bdd295bfc9ba2b6a69e1a386
Princess-Nnamdi <pnnamdi20200@gmail.com>
2023-07-27 11:23:26 -0700

    monty

--- 0-main.c
+++ 0-main.c
@@ -1,17 +1,9 @@
 #include "monty.h"
 
-/**
- * main - Entry point
- * argc: number of arguements
- * argv: array of arguements
- **/
-
 global_t vglo;
 
 /**
  * free_vglo - frees the global variables
- *
- * Return: no return
  */
 void free_vglo(void)
 {
@@ -20,42 +12,84 @@ void free_vglo(void)
 	fclose(vglo.fd);
 }
 
-#define MAX_LINE_LENGTH 1024
-int main(int argc, char *argv[])
+/**
+ * start_vglo - initializes the global variables
+ * @fd: file descriptor
+ */
+void start_vglo(FILE *fd)
 {
-	FILE *file;
-	char line[MAX_LINE_LENGTH];
-	unsigned int line_number = 0;
-	stack_t *stack = NULL;
-	size_t line_length;
+	vglo.lifo = 1;
+	vglo.cont = 1;
+	vglo.arg = NULL;
+	vglo.head = NULL;
+	vglo.fd = fd;
+	vglo.buffer = NULL;
+}
 
-	if (argc != 2)
+/**
+ * check_input - checks if the file exists and if the file can
+ * be opened
+ * @argc: number of arguements
+ * @argv: array of arguments
+ * Return: file struct
+ */
+FILE *check_input(int argc, char *argv[])
+{
+	FILE *fd;
+
+	if (argc == 1 || argc > 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(argv[1], "r");
-	if (file == NULL)
+
+	fd = fopen(argv[1], "r");
+
+	if (fd == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (fgets(line, MAX_LINE_LENGTH, file) != NULL)
-	{
-		line_number++;
-		line_length = strlen(line);
 
-		if (line_length > 0 && line[line_length - 1] == '\n')
-			line[line_length - 1] = '\0';
-		if (parse_line(line, &stack, line_number) == -1)
-		{
-			free_stack(stack);
-			fclose(file);
-			exit(EXIT_FAILURE);
-		}
-	}
-	free_stack(stack);
-	fclose(file);
+	return (fd);
+}
 
-	return (0);
+#define MAX_LINE_LENGTH 1024
+int main(int argc, char *argv[])
+{
+        FILE *file;
+        char line[MAX_LINE_LENGTH];
+        unsigned int line_number = 0;
+        stack_t *stack = NULL;
+        size_t line_length;
+
+        if (argc != 2)
+        {
+                fprintf(stderr, "USAGE: monty file\n");
+                exit(EXIT_FAILURE);
+        }
+        file = fopen(argv[1], "r");
+        if (file == NULL)
+        {
+                fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+                exit(EXIT_FAILURE);
+        }
+        while (fgets(line, MAX_LINE_LENGTH, file) != NULL)
+        {
+                line_number++;
+                line_length = strlen(line);
+
+                if (line_length > 0 && line[line_length - 1] == '\n')
+                        line[line_length - 1] = '\0';
+                if (parse_line(line, &stack, line_number) == -1)
+                {
+                        free_stack(stack);
+                        fclose(file);
+                        exit(EXIT_FAILURE);
+                }
+        }
+        free_stack(stack);
+        fclose(file);
+
+        return (0);
 }
--- monty.h
+++ monty.h
@@ -81,5 +81,6 @@ void stack_function(stack_t **stack, unsigned int line_number);
 stack_t *add_dnodeint_end(stack_t **head, const int n);
 stack_t *add_dnodeint(stack_t **head, const int n);
 void free_dlistint(stack_t *head);
+char *_strtoky(char *s, char *d);
 void free_vglo(void);
 #endif
